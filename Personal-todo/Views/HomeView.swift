a58fe4597ad29a0fe1a4a66481f18553e1a6c005
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-25 22:41:50 +0400

    + HomeView, Projects View - Listing Projects and Project items

---
+++ Personal-todo/Views/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  Personal-todo
+//
+//  Created by Irakli Sokhaneishvili on 25.01.22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
