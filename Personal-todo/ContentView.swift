79f1a0a8f1b1e3009ef7cca6d3ccc52e56f761c6
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-25 22:18:10 +0400

    first commit

---
+++ Personal-todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Personal-todo
+//
+//  Created by Irakli Sokhaneishvili on 25.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Personal-todo/Personal_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_todoApp.swift
+//  Personal-todo
+//
+//  Created by Irakli Sokhaneishvili on 25.01.22.
+//
+
+import SwiftUI
+
+@main
+struct Personal_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
