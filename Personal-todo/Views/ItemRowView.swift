bf22e1054b75ffe62325f884557c6550bbfa5cc5
Irakli Sokhaneishvili <irakli.swift@gmail.com>
2022-01-26 14:08:44 +0400

    + Edit Items

---
+++ Personal-todo/Views/ItemRowView.swift
@@ -0,0 +1,24 @@
+//
+//  ItemRowView.swift
+//  Personal-todo
+//
+//  Created by Irakli Sokhaneishvili on 26.01.22.
+//
+
+import SwiftUI
+
+struct ItemRowView: View {
+    @ObservedObject var item: Item
+    
+    var body: some View {
+        NavigationLink(destination: EditItemView(item: item)) {
+            Text(item.itemTitle)
+        }
+    }
+}
+
+struct ItemRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRowView(item: Item.example)
+    }
+}
